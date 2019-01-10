7b87face8b02ca1e32e9cd9ea3a4293f29e7f13c
Walley Yang <walleyyang@gmail.com>
2019-01-09 19:20:42 -0500

    Added icon to email tree view

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -8,10 +8,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.web.WebView;
 
 public class MainController implements Initializable {
@@ -21,6 +26,10 @@ public class MainController implements Initializable {
 
   @FXML
   private Button Button1;
+  
+  @FXML
+  private TreeView<String> emailFoldersTreeView;
+  private TreeItem<String> root = new TreeItem<String>();
 
   @FXML
   private TableColumn<EmailMessage, String> subjectCol;
@@ -67,6 +76,54 @@ public class MainController implements Initializable {
       }
       
     });
+    
+    emailFoldersTreeView.setRoot(root);
+    
+    root.setValue("test@gmail.com");
+    root.setGraphic(resolveIcon(root.getValue()));
+    
+    TreeItem<String> Inbox = new TreeItem<String>("Inbox", resolveIcon("Inbox"));
+    TreeItem<String> Sent = new TreeItem<String>("Sent", resolveIcon("Sent"));
+      TreeItem<String> Subitem1 = new TreeItem<String>("Subitem1", resolveIcon("Subitem1"));
+      TreeItem<String> Subitem2 = new TreeItem<String>("Subitem2", resolveIcon("Subitem2"));
+      Sent.getChildren().addAll(Subitem1, Subitem2);
+    TreeItem<String> Spam = new TreeItem<String>("Spam", resolveIcon("Spam"));
+    TreeItem<String> Trash = new TreeItem<String>("Trash", resolveIcon("Trash"));
+    
+    root.getChildren().addAll(Inbox, Sent, Spam, Trash);
+    root.setExpanded(true);
+  }
+  
+  private Node resolveIcon(String treeitemValue) {
+    String lowerCaseTreeItemValue = treeitemValue.toLowerCase();
+    ImageView returnIcon;
+    
+    try {
+      if (lowerCaseTreeItemValue.contains("inbox")) {
+        returnIcon = new ImageView(new Image(getClass().getResourceAsStream("images/inbox.png")));
+      }
+      else if (lowerCaseTreeItemValue.contains("sent")) {
+        returnIcon = new ImageView(new Image(getClass().getResourceAsStream("images/sent.png")));
+      }
+      else if (lowerCaseTreeItemValue.contains("spam")) {
+        returnIcon = new ImageView(new Image(getClass().getResourceAsStream("images/spam.png")));
+      }
+      else if (lowerCaseTreeItemValue.contains("@")) {
+        returnIcon = new ImageView(new Image(getClass().getResourceAsStream("images/email.png")));
+      }
+      else {
+        returnIcon = new ImageView(new Image(getClass().getResourceAsStream("images/folder.png")));
+      }
+    } catch (NullPointerException e) {
+      System.out.println("Invalid image location");
+      e.printStackTrace();
+      return new ImageView();
+    }
+    
+    returnIcon.setFitHeight(16);
+    returnIcon.setFitWidth(16);
+    
+    return returnIcon;
   }
 
 }
